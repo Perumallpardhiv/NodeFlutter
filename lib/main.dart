b56f8d01e07e203f8fd1c8ae725bdb8243d0db64
Perumallpardhiv <lit2021001@iiitl.ac.in>
2023-06-03 00:41:56 +0530

    Keeping user logged In

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutterfrontend/home.dart';
 import 'package:flutterfrontend/register.dart';
+import 'package:jwt_decoder/jwt_decoder.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MainApp());
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  runApp(MainApp(token: prefs.getString('token')));
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  final token;
+  const MainApp({@required this.token, super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Register(),
+      home: JwtDecoder.isExpired(token) == false
+          ? Home(token: token)
+          : const Register(),
     );
   }
 }
